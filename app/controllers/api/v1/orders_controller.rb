d5dd041fcaec43263552a9e72aeb5d619cd79f9a
Sam Standing <samzstanding@gmail.com>
2018-05-21 14:02:23 -0500

    successfully posting with new schema

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,19 +1,25 @@
 module Api::V1
     class OrdersController < ApplicationController
+      before_action :set_status
       def index
         @orders = Order.all
         render json: @orders
       end
 
       def create 
-        @order = Order.create(order_params)
+        @order = @status.orders.create! (order_params)
         render json: @order
+
       end
 
       private
 
+      def set_status
+        @status = Status.find(params[:status_id])
+      end
+
       def order_params
-        params.require(:order).permit(:pickup, :delivery, :when, :vehicle, :price)
+        params.require(:order).permit(:pickup, :delivery, :when, :vehicle, :price, :status_id)
       end
 
     end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,7 @@
 class Order < ApplicationRecord
     belongs_to :status
+    # before_save :default_values
+    # def default_values
+    #     self.status_id ||= 1
+    # end
 end
